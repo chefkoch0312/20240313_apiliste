226e076b45da344988c5c07156e0d62317f2bcaa
Kai Dombrowski <kai.dombrowski@web.de>
2024-03-13 12:34:31 +0100

    mit api aufruf

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export default function MyComponent({ api }) {
+    const openLink = () => {
+        window.open(api.Link, '_blank');
+    };
+
     return (
         <div className="container">
             <div className="card">
@@ -9,7 +13,7 @@ export default function MyComponent({ api }) {
                     <p className="card-text">Kategorie: {api.Category}</p>
                     <p className="card-text">Beschreibung: {api.Description}</p>
                     <p className="card-text">Auth: {api.Auth ? api.Auth : "offen"}</p>
-                    <button className="btn btn-primary">Mehr Info</button>
+                    <button className="btn btn-primary" onClick={openLink}>API aufrufen</button>
                 </div>
             </div>
         </div>
